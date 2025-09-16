541f2a1ab33f07f15774c82489f4506a35e171c6
Vazzala16 <154043993+Vazzala16@users.noreply.github.com>
2025-09-16 12:00:55 +0530

    Create calculator.py

---
+++ Day4/calculator.py
@@ -0,0 +1,16 @@
+num1 = 10
+num2 = 5
+
+def addition():
+    a= num1 +num2
+    print(a)
+def sub():
+    b= num1-num2
+    print(b)
+def mul():
+    c= num1*num2
+    print(c)
+
+addition()
+sub()
+mul()
